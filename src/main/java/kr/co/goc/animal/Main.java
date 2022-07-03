de0334c3775af776bdb43975b650f6adfd1a3cd0
mii08 <dlxl1005@naver.com>
2022-07-03 14:35:06 +0900

    .

--- src/main/java/kr/co/goc/animal/Main.java
+++ src/main/java/kr/co/goc/animal/Main.java
@@ -5,6 +5,6 @@ import kr.co.goc.animal.animalController.AniamlController;
 public class Main {
 	public static void main(String[] args) {
 		new AniamlController();
-		System.out.println("hello");
+		
 	}
 }
